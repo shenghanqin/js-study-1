d22ade18eeb39c2b6298fb699bc67c81542af7e8
盛瀚钦 <shenghanqin@smartisan.com>
2016-01-04 10:48:40 +0800

    学习学习

--- julyjs/my-july.js
+++ julyjs/my-july.js
@@ -50,9 +50,9 @@ julyJs.init = function () {
     // 合并 参数
     this.option = this.extend(this.option, this.config);
 
-    // 判断浏览器信息
-    this.browser.coreInit();
-
+    //// 判断浏览器信息
+    //this.browser.coreInit();
+    //
     // bad js
     this.badJs();
 };
@@ -78,13 +78,16 @@ julyJs.extend = function () {
         for (; i < length; i++) {
             if ((options = arguments[i]) != null) {
                 for (var name in options) {
-                    var copy = options[name];
-                    if (target === copy) {
-                        continue;
-                    }
+                    if (options.hasOwnProperty(name)) {
+                        var copy = options[name];
+                        if (target === copy) {
+                            continue;
+                        }
 
-                    if (copy !== undefined) {
-                        target[name] = copy;
+                        if (copy !== undefined) {
+                            target[name] = copy;
+                        }
+
                     }
                 }
             }
@@ -93,7 +96,7 @@ julyJs.extend = function () {
 
     return target;
 
-}
+};
 
 // 加载 js
 //# 加载js文件（支持跨域）
@@ -131,6 +134,7 @@ julyJs.loadJs = function (src, option) {
 
     script.onreadystatechange = function () {
         var state = this.readyState;
+        console.log(state);
         if (state === 'loaded' || state ==='complete') {
             script.onreadystatechange = null;
             // 清除超时事件
@@ -147,5 +151,120 @@ julyJs.loadJs = function (src, option) {
 
         // 触发错误
         options.onerror();
-    }, options.time());
+    }, options.timeout);
+};
+
+//构造url参数
+//# 构造url参数
+julyJs.param = function (option, isCode, url) {
+    var array = [];
+    // 连接符
+    var joins = '';
+    // tmp
+    var value = '';
+
+    url = url || '';
+    for (var o in  option){
+        if (option.hasOwnProperty(o) && o) {
+            if (isCode) {
+                value = encodeURIComponent(option[o] || '');
+            } else {
+                value = (option[o] || '');
+            }
+
+            array.push(o + '=' + value);
+
+        }
+    }
+
+    if (url) {
+        joins = url.indexOf('?') > -1 ? '&' : '?';
+    }
+
+    return url + joins + array.join('&');
+};
+
+// 二维码
+julyJs.qr = function () {
+    console.log(vl);
+};
+
+//1、运行时错误，例如无效的对象引用或安全限制， 2、下载错误，如图片， 3、在IE9中，获取多媒体数据失败也会引发
+//window 对象都支持 onerror ， <css> 和 <iframe> 不支持onerror。
+//对于引用外部js文件中的错误，Webkit和Mozilla类浏览器会篡改原始的错误信息，导致最后onerror获取到的三个入参为： “Script error.”,”", 0
+julyJs.badJs = function (postUrl) {
+    var errorUrl = this.option.errorUrl;
+    if (!errorUrl) return;
+
+    // 上报
+    window.onerror = function (msg, url, line) {
+        var option = {
+            browser: window.navigator.userAgent,
+            referrer: document.referrer,
+            url: url,
+            line: line
+        };
+        var iframe = document.createElement('iframe');
+        iframe.src = julyJs.param(option, true, errorUrl);
+        iframe.onload = function () {
+            document.body.removeChild(iframe);
+        };
+        iframe.style.display = 'none';
+
+        //julyJs.loadJs( errorUrl ); 此处不能 用 js 方式！万一请求源错误，或者 返回错误js代码，会导致死循环。
+        document.body.appendChild(iframe);
+        console.log('js error', option);
+
+
+    };
+
+
+
+};
+//############ 一些事件 event
+
+julyJs.event = {
+    //# 事件禁止
+    //@type : 类型
+    //@area : 范围
+    disable: function (type, area) {
+        area = area || document.body;
+        switch (type) {
+            // 选择文字
+            case 'select':
+                area.onselectstart = function () {
+                    return false;
+                };
+                break;
+            case 'rightMouse':
+                area.oncontextmenu = function () {
+                    return false;
+                };
+                break;
+            case 'copy':
+                area.oncopy = function () {
+                    return false;
+                };
+                break;
+            case 'paste':
+                area.onpaste = function () {
+                    return false;
+                };
+            break
+        }
+    },
+    // 输入框 限定字数
+    //@inputDom 输入框
+    //@numDom 数字
+    //@maxDum
+    textCount: function (inputDom, numDom, maxNum) {
+        var text = inputDom.value || '';
+        var len = text.length;
+        var overplus = maxNum - len;
+        if (overplus < 0) {
+            inputDom.value = text.slice(0, maxNum);
+            overplus = 0;
+        }
+        numDom.innerHTML = overplus;
+    }
 };
